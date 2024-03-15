55aa9b414ec0e62aa51fc0b2cf7f9009fece0db8
Sophed <65602967+Sophed@users.noreply.github.com>
2024-03-15 21:24:41 +0000

    :recycle: Remake info print

--- out.go
+++ out.go
@@ -1,37 +1,63 @@
 package main
 
-func printInfo(profile *Profile) {
-
-	uuid := hyphen(profile.UUID)
-
-	print("┌")
-	line(len(uuid) + 9)
-	println("┐")
-
-	print("│ Profile  ", profile.Name)
-
-	for i := 0; i < len(uuid)+1-len(profile.Name); i++ {
-		print(" ")
-	}
-	println("│")
-
-	println("│ UUID    ", uuid, "│")
-
-	print("└") // ├
-	line(len(uuid) + 9)
-	println("┘") // ┤
-
-}
-
-func line(n int) {
-	for i := 0; i < n+2; i++ {
-		print("─")
-	}
-}
-
 func hyphen(uuid string) string {
 	if len(uuid) != 32 {
 		return ""
 	}
 	return uuid[:8] + "-" + uuid[8:12] + "-" + uuid[12:16] + "-" + uuid[16:20] + "-" + uuid[20:]
+}
+
+// │ ┌ ┐ └ ┘ ─ ┤ ├
+
+func printInfo(profile *Profile) {
+
+	uuid := hyphen(profile.UUID)
+
+	m := map[string]string{
+		"Username": profile.Name,
+		"UUID":     uuid,
+	}
+	prettyPrint(m)
+
+}
+
+func line(n int) string {
+	var s string
+	for i := 0; i < n; i++ {
+		s += "─"
+	}
+	return s
+}
+
+const padding = 1
+
+func prettyPrint(m map[string]string) {
+	longestK := 0
+	for k := range m {
+		if len(k) > longestK {
+			longestK = len(k)
+		}
+	}
+	longestV := 0
+	for _, v := range m {
+		if len(v) > longestV {
+			longestV = len(v)
+		}
+	}
+
+	println("┌" + line(longestK+longestV+padding*2+1) + "┐")
+	for k, v := range m {
+		print("│ ", k)
+		padDiff(k, longestK+padding)
+		print(v)
+		padDiff(v, longestV+padding)
+		println("│")
+	}
+	println("└" + line(longestK+longestV+padding*2+1) + "┘")
+}
+
+func padDiff(s string, n int) {
+	for i := 0; i < n-len(s); i++ {
+		print(" ")
+	}
 }
